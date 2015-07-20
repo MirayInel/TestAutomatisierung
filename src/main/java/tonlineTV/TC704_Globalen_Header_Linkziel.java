0530f9efe737906d025a20f9d2aef46f408b220b
m.inel <miray@gmx.de>
2015-07-20 08:32:34 +0200

    TC704 GH Linkziel prüfen wurde angepasst.

--- src/main/java/tonlineTV/TC704_Globalen_Header_Linkziel.java
+++ src/main/java/tonlineTV/TC704_Globalen_Header_Linkziel.java
@@ -1,59 +1,47 @@
 package tonlineTV;
 
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-public class TC704_Globalen_Header_Linkziel extends TOnlineTV{
-	
-	@Override
-	public void runTest(){
-		
+public class TC704_Globalen_Header_Linkziel extends TOnlineTV {
+
+	public void runTest() {
+
 		WebElement globHeader = driver.findElement(TVElements.gh_globhead);
-		String currentTab = driver.getWindowHandle().toString();
-		System.out.println("currentTab" + currentTab);
-		
+
 		if (globHeader.isDisplayed()) {
 			driver.findElement(gh_mehr).click();
-//			System.out.println("handle" + driver.getWindowHandle());
-			System.out.println("handles" + driver.getWindowHandles());
+			System.out.println(driver.getWindowHandles());
+			
 
+
+			int i = 0;
 			for (By elem : l_GlobHeadElem) {
-				int i = 0;
 				WebElement globHeadElem = driver.findElement(elem);
 				globHeadElem.click();
+
+				String[] currentWindows = driver.getWindowHandles().toArray(new String[driver.getWindowHandles().size()]);
+				driver.switchTo().window(currentWindows[i+1]);
+				 if (driver.getCurrentUrl() == url_GlobHeadElem.get(i)) {
 				
-				if(elem == gh_tonline){
-					// get 2nd window and compare Urls
-				System.out.println(driver.getWindowHandles().toArray());
-				//currentTab = ...2nd wind.
-				}
-				
-				if (driver.getCurrentUrl() == url_GlobHeadElem.get(i)) {
-					System.out.println(elem + "URL stimmt überein");
-					
-					
-				} else {
-					System.out.println(elem + "URL stimmt nicht überein. Folgende URL wird aufgerufen" + driver.getCurrentUrl());
-				}
+					 System.out.println(driver.switchTo().window(currentWindows[i+1]).getCurrentUrl());
+				 System.out.println(elem + "URL stimmt überein");
+				 testStatus = true;
 			
-				System.out.println("handle in for" +driver.getWindowHandle());
-				System.out.println("handles in for" +driver.getWindowHandles());
-				driver.getWindowHandle();
+				 } else {
+				 System.out.println(elem + "URL stimmt nicht überein. Folgende URL wird aufgerufen" + driver.getCurrentUrl());
+				 testStatus=false;
+				 } 
+				
+				driver.switchTo().window(currentWindows[0]);
 //				driver.get(TVElements.url_tvStartseite);
 				i++;
 //				System.out.println(l_GlobHeadElem.get(i));
 //				System.out.println(driver.getWindowHandles());
-				
-					
-				}
-			}
-			
-		else {
-			System.out.println("Globalen Header wird nicht angezeigt");
-		}
-		
-		
-	}
 
+			}
+
+		}
+	}
 }
+
